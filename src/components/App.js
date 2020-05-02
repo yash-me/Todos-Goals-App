db00b4bab1b6adacb9cf11f248930c36d7a000c8
kakubotics <yashkhandelwal27@gmail.com>
2020-05-02 22:19:10 +0530

    First commit

---
+++ src/components/App.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import ConnectedTodos from './Todos'
+import ConnectedGoals from './Goals'
+import { connect } from 'react-redux'
+import {
+
+  handleInitialData
+} from '../actions/shared'
+
+class App extends React.Component {
+
+  componentDidMount (){
+
+      const { dispatch } = this.props;
+
+      dispatch(handleInitialData())
+
+      // store.subscribe(() => this.forceUpdate())
+  }
+
+  render(){
+
+      // const { store } = this.props
+      // const {loading} = store.getState()
+
+      if(this.props.loading === true){
+
+          return <h3>Loading...</h3>
+      }
+
+      return(
+
+          <div>
+              <ConnectedTodos/>
+              <ConnectedGoals/>
+          </div>
+
+      )
+  }
+}
+
+
+
+
+export default connect((state) => ({
+
+  loading:state.loading
+}))(App)
